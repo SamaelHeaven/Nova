0c098bdb3b9adfc09cd2f8e763d04ec1589d1320
Samuel Paradis <samuelparadisinfo@gmail.com>
2024-04-18 21:25:14 -0400

    Refactor: Minor changes

--- declarations/nova/escape.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function escape(unsafe: {
-    toString(): string;
-}): string;
--- javascript/nova/escape.js
+++
@@ -1,3 +0,0 @@
-export function escape(unsafe) {
-    return unsafe.toString().replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&#039;");
-}
